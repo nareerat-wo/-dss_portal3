bcce6254bd74eb83a244e09f32d82d21e5902025
NareeratWo <nareerat.wongwan@scb.co.th>
2019-12-25 14:43:33 +0700

    update db

--- portal_project/settings.py
+++ portal_project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'portal_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'sql_server.pyodbc',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
